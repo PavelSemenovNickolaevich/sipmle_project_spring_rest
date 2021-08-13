c6ea54d365ae48c42c6379b578c0942aaef0eaf6
pavel <natitshow@gmail.com>
2021-08-13 21:51:01 +0300

    Получение списка всех работников

---
+++ src/main/java/com/pavel/spring/rest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.pavel.spring.rest.service;
+
+import com.pavel.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public void deleteEmployee(int id);
+
+    public Employee getEmployee(int id);
+}
